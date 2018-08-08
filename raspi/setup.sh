3109477b8ce2eda0ee0b216f91fed721380329fb
Justin Swanson <geeksunny@gmail.com>
2018-08-07 20:59:35 -0500

    Setup script fix and comments

--- raspi/setup.sh
+++ raspi/setup.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# Prevent the script from being run on non-ARM CPUs.
 UNAME=$(uname -m)
 if [[ $UNAME != arm* ]]
 then
@@ -10,9 +11,11 @@ fi
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
+# Use apt-get to update the system and install required software.
 echo "Updating system and installing software dependencies."
 sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install -y libasound2-dev libudev-dev sox
 
+# If the npm command cannot be resolved, install Node.js
 if ! [[ "$(command -v npm)" ]]
 then
     echo "npm command not found. Installing Node.js"
@@ -20,8 +23,7 @@ then
     then
         wget https://nodejs.org/dist/v10.8.0/node-v10.8.0-linux-armv6l.tar.xz
         tar -xvf node-v10.8.0-linux-armv6l
-        cd node-v10.8.0-linux-armv6l
-        sudo cp -R * /usr/local/
+        sudo cp -R node-v10.8.0-linux-armv6l/* /usr/local/
     else
         curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
         sudo apt install -y nodejs
@@ -30,9 +32,11 @@ else
     echo "npm is already installed. Skipping installation."
 fi
 
+# Install PiMidiBox software to be globally accessible via $PATH.
 echo "Installing PiMidiBox module globally."
 sudo npm install -g ..
 
+# Install and activate a systemd service starting the software at boot.
 echo "Installing PiMidiBox systemd service."
 sudo cp ./pimidibox.service /etc/systemd/system/pimidibox.service
 sudo systemctl start pimidibox
